3dc11fbdf4603f927d68630a515e799166ac6179
Mattia3 <mcortinovis9@gmail.com>
2021-11-09 16:25:56 +0100

    Esercizio non completato

---
+++ js/script.js
@@ -0,0 +1,26 @@
+Vue.config.devtools = true;
+
+new Vue({
+  el: "#root",
+  data:{
+   apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+   userEmail: [],
+   
+  },
+
+  methods:{
+    startCreateEmail(){
+      
+      for (let i = 0; i < 10; i++) {
+        axios.get(this.apiUrl).then((ajaxResponse) => {
+          this.userEmail.push(ajaxResponse.data.response) 
+          if(this.userEmail.length >= 10){
+            console.log(this.userEmail)
+              
+          }
+        })
+      }
+    }
+  }
+
+})
